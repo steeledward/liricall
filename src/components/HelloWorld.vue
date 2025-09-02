c290b376d961fb14da6e23a06310850978574cc1
Steel Edward Vazquez George <sevg@Steels-MacBook-Pro.local>
2025-09-02 15:11:37 -0600

    Embedded Appsheet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,10 @@
 <template>
-  <v-container class="fill-height" max-width="900">
-      <v-img
-        class="mb-4"
-        height="100vh"
-        src="https://www.appsheet.com:443/fsimage.png?appid=dc5de00c-91a8-4e58-998c-4ec2520f7ffe&datasource=google&filename=DocId%3D1jnBBD1nBVvrhrnAq27uwqt57sLw7X-0M&signature=f19497b150860024b07b9409a29e05ec231d70b7e109222c8e4d0a35b4eb5546&tableprovider=google&userid=803038567"
-      />
+  <v-container class="fill-height" fluid>
+      <iframe
+        src="https://www.appsheet.com/start/b704b4e4-6206-4e16-8389-aafa864cff2a"
+        frameborder="0"
+        style="width:100%; height:100%;"
+        allowfullscreen
+      ></iframe>
   </v-container>
 </template>
