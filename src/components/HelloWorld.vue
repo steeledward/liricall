552d3107d7f5099123f15bdae1431d0924d34734
Steel Edward Vazquez George <sevg@Steels-MacBook-Pro.local>
2025-09-02 13:58:35 -0600

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="fill-height" max-width="900">
+      <v-img
+        class="mb-4"
+        height="100vh"
+        src="https://www.appsheet.com:443/fsimage.png?appid=dc5de00c-91a8-4e58-998c-4ec2520f7ffe&datasource=google&filename=DocId%3D1jnBBD1nBVvrhrnAq27uwqt57sLw7X-0M&signature=f19497b150860024b07b9409a29e05ec231d70b7e109222c8e4d0a35b4eb5546&tableprovider=google&userid=803038567"
+      />
+  </v-container>
+</template>
