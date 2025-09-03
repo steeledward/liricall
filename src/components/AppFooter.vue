c0aed8ab473dc11a3b95cfe2c9a121e9280e224f
Steel Edward Vazquez George <sevg@Steels-MacBook-Pro.local>
2025-09-03 17:14:44 -0600

    Footer with links

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,8 +3,49 @@
     app
     height="40"
   >
+    <a
+      v-for="item in items"
+      :key="item.title"
+      class="d-inline-block mx-2 social-link"
+      :href="item.href"
+      rel="noopener noreferrer"
+      target="_blank"
+      :title="item.title"
+    >
+      <v-icon
+        :icon="item.icon"
+        :size="item.icon === '$vuetify' ? 24 : 16"
+      />
+    </a>
+
+    <div
+      class="text-caption text-disabled"
+      style="position: absolute; right: 16px;"
+    >
+      &copy; {{ (new Date()).getFullYear() }} <span class="d-none d-sm-inline-block">Liricall, LLC</span>
+    </div>
   </v-footer>
 </template>
+
+<script setup lang="ts">
+  const items = [
+    {
+      title: 'Administration',
+      icon: 'mdi-shield-star-outline',
+      href: 'https://www.appsheet.com/start/dc5de00c-91a8-4e58-998c-4ec2520f7ffe',
+    },
+    {
+      title: 'YouTube',
+      icon: 'mdi-youtube',
+      href: 'https://www.youtube.com/@liricalling',
+    },
+    {
+      title: 'Facebook',
+      icon: 'mdi-facebook',
+      href: 'https://www.facebook.com/profile.php?id=61579332326557',
+    },
+  ]
+</script>
 
 <style scoped lang="sass">
   .social-link :deep(.v-icon)
